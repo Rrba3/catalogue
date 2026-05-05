a611bb6d5f072f38b4602da61a61727ed81ed900
Ahmad <you@example.com>
2026-05-05 20:18:52 +0200

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import products from "@/data/products.json";
+import productsData from "@/data/products.json";
 import CatalogApp from "./components/CatalogApp";
+import { Product } from "./types";
+
+const products = productsData as Product[];
 
 export default function Home() {
   return <CatalogApp products={products} />;
